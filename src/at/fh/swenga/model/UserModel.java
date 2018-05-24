9fab2d8dde50f2789ee8df85b3f4baedd67c1e07
nadineneumann <nadine.neumann@edu.fh-joanneum.at>
2018-05-24 18:25:18 +0200

    Source Folder für Model

---
+++ src/at/fh/swenga/model/UserModel.java
@@ -0,0 +1,44 @@
+package at.fh.swenga.model;
+
+import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@Entity
+@Table(name = "users")
+public class UserModel implements java.io.Serializable {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY) //value is set by table itself that should be unique
+	private int id;
+	
+	@Column(name = "username", unique = true, nullable = false, length = 45)
+	private String userName;
+	
+	@Column(name = "password", nullable = false, length = 60)
+	private String password;
+	
+	@Column(name = "disabled", nullable = false)
+	private boolean disabled;
+	
+	private String firstName;
+	
+	private String lastName;
+	
+	private Date dayOfBirth;
+	
+	
+	
+}
