21ad385466538ecc70d20a1154085645296a61f6
JakobArneitz <Jakob.Arneitz@gmail.com>
2018-06-17 17:41:27 +0200

    sortable

--- src/at/fh/swenga/jpa/dao/AttackDao.java
+++ src/at/fh/swenga/jpa/dao/AttackDao.java
@@ -1,18 +1,17 @@
 package at.fh.swenga.jpa.dao;
 
+
 import java.util.List;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.TypedQuery;
 
-import org.springframework.security.access.annotation.Secured;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
 import at.fh.swenga.jpa.model.AttackModel;
-import at.fh.swenga.jpa.model.TopicModel;
-import at.fh.swenga.jpa.model.TypeModel;
+
 
 @Repository
 @Transactional
@@ -33,7 +32,7 @@ public class AttackDao {
 
 	public List<AttackModel> searchAttack(String searchString) {
 		TypedQuery<AttackModel> typedQuery = entityManager.createQuery(
-				"select e from AttackModel e where e.name like :search or e.category like :search order by e.name",
+				"select e from AttackModel e where e.name like :search order by e.name asc",
 				AttackModel.class);
 		typedQuery.setParameter("search", "%" + searchString + "%");
 		List<AttackModel> typedResultList = typedQuery.getResultList();
